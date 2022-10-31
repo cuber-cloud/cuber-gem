ad89e5ebea848b06aed405c554ff056f5efe3f84
Marco Colli <collimarco91@gmail.com>
2022-10-31 14:02:05 +0100

    Add term option to proc (grace period in seconds for shutdown)

--- lib/cuber/cuberfile_parser.rb
+++ lib/cuber/cuberfile_parser.rb
@@ -64,8 +64,8 @@ module Cuber
       @migrate = { cmd: cmd, check: check }
     end
 
-    def proc name, cmd, scale: 1, env: {}
-      @procs[name] = { cmd: cmd, scale: scale, env: env }
+    def proc name, cmd, scale: 1, term: 60, env: {}
+      @procs[name] = { cmd: cmd, scale: scale, term: term, env: env }
     end
 
     def cron name, schedule, cmd
--- lib/cuber/cuberfile_validator.rb
+++ lib/cuber/cuberfile_validator.rb
@@ -81,6 +81,7 @@ module Cuber
         @errors << "proc \"#{procname}\" name can only include lowercase letters" if procname !~ /\A[a-z]+\z/
         @errors << "proc \"#{procname}\" command must be present" if proc[:cmd].to_s.strip.empty?
         @errors << "proc \"#{procname}\" scale must be a positive number" unless proc[:scale].is_a?(Integer) && proc[:scale] > 0
+        @errors << "proc \"#{procname}\" term must be a positive number" unless proc[:term].is_a?(Integer) && proc[:term] > 0
         proc[:env].each do |key, value|
           @errors << "proc \"#{procname}\" env name can only include uppercase letters, digits or underscores" if key !~ /\A[A-Z_]+[A-Z0-9_]*\z/
         end
